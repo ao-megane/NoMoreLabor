f96d7309e36393c2a7160dd325d46fa942f7c8f4
aomeg <aomegane0716@outlook.jp>
2017-12-07 16:12:45 +0900

    aaa

--- NoMoreLabor/NoMoreLabor/main.cpp
+++ NoMoreLabor/NoMoreLabor/main.cpp
@@ -21,10 +21,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Input input;
 	Dot center;
 	Dot decoi;
+	Dot m;
 	Motors motor;
-
 	center.Set(DISP_WIDTH / 2, DISP_HEIGHT / 2);
-	//motor.Set(0.5, 0.5, 1.0);
 	
 	MenuElement_t MenuElement[3] = {
 		{ 100,300,"スタート1(仮)" },
@@ -46,73 +45,73 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	}
 	/*----------------ここまで---------------------*/
 
-	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
-		input.Updata();
-
-		switch (sceFlag){//シミュレータの流れを管理するスイッチ
-		case 0://タイトル
-			//計算
-			if (input.GetKey(KEY_INPUT_DOWN) == 1) {
-				stageFlag = (stageFlag + 1) % 3;
-			}
-			if (input.GetKey(KEY_INPUT_UP) == 1) {
-				stageFlag = (stageFlag + 2) % 3;
-			}
-			if (input.GetKey(KEY_INPUT_ESCAPE) == 1) {
-				stageFlag = 2;
-				MenuElement[0].x = 200;
-				MenuElement[1].x = 200;
-				MenuElement[2].x = 100;
-			}
-
-			if (input.GetKey(KEY_INPUT_DOWN) == 1 || input.GetKey(KEY_INPUT_UP) == 1) {
-				for (int i = 0; i < 3; i++) {
-					if (i == stageFlag) {
-						MenuElement[i].x = 100;
-					}
-					else {
-						MenuElement[i].x = 200;
-					}
-				}
-			}
-			
-			if (input.GetClick() == 1) sceFlag = stageFlag;
-
-			break;
-		case 1://リアルモード
-			break;
-		case 2://アンリアルモード
-			break;
-		case 3://クレジット
-			break;
-		case 4://終了
-			break;
-		default:
-			break;
-		}
-		
-		//描画
-		for (int i = 0; i < 3; i++) {
-			//DrawFormatString(MenuElement[i].x, MenuElement[i].y, GetColor(225, 225, 225), MenuElement[i].name);
-			DrawStringToHandle(MenuElement[i].x, MenuElement[i].y, MenuElement[i].name, GetColor(225, 225, 225), tanuki);
-		}
-		if (CheckHitKey(KEY_INPUT_DELETE)) break;
-	}
-	/*------------------------------------------------------------------------------------------------------------------------------------------------*/
 	//while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
-	//	
-	//	decoi = (input.GetMouse() - center);
-	//	decoi.Updata();
+	//	input.Updata();
 
-	//	motor.Calc(decoi);
+	//	switch (sceFlag){//シミュレータの流れを管理するスイッチ
+	//	case 0://タイトル
+	//		//計算
+	//		if (input.GetKey(KEY_INPUT_DOWN) == 1) {
+	//			stageFlag = (stageFlag + 1) % 3;
+	//		}
+	//		if (input.GetKey(KEY_INPUT_UP) == 1) {
+	//			stageFlag = (stageFlag + 2) % 3;
+	//		}
+	//		if (input.GetKey(KEY_INPUT_ESCAPE) == 1) {
+	//			stageFlag = 2;
+	//			MenuElement[0].x = 200;
+	//			MenuElement[1].x = 200;
+	//			MenuElement[2].x = 100;
+	//		}
 
-	//	//DrawMouse(m);
-	//	//DrawMouse(decoi.Rotate(PI / 2);
-	//	motor.Draw();
-	//	DrawLineByDot(input.GetMouse(), center, BLUE);
-	//	DrawCircle(center.GetX(), center.GetY(), 3, RED, true);
+	//		if (input.GetKey(KEY_INPUT_DOWN) == 1 || input.GetKey(KEY_INPUT_UP) == 1) {
+	//			for (int i = 0; i < 3; i++) {
+	//				if (i == stageFlag) {
+	//					MenuElement[i].x = 100;
+	//				}
+	//				else {
+	//					MenuElement[i].x = 200;
+	//				}
+	//			}
+	//		}
+	//		
+	//		if (input.GetClick() == 1) sceFlag = stageFlag;
+
+	//		break;
+	//	case 1://リアルモード
+	//		break;
+	//	case 2://アンリアルモード
+	//		break;
+	//	case 3://クレジット
+	//		break;
+	//	case 4://終了
+	//		break;
+	//	default:
+	//		break;
+	//	}
 	//	
+	//	//描画
+	//	for (int i = 0; i < 3; i++) {
+	//		//DrawFormatString(MenuElement[i].x, MenuElement[i].y, GetColor(225, 225, 225), MenuElement[i].name);
+	//		DrawStringToHandle(MenuElement[i].x, MenuElement[i].y, MenuElement[i].name, GetColor(225, 225, 225), tanuki);
+	//	}
+	//	if (CheckHitKey(KEY_INPUT_DELETE)) break;
 	//}
+	/*------------------------------------------------------------------------------------------------------------------------------------------------*/
+	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
+		
+		decoi = (input.GetMouse() - center);
+		decoi.Updata();
+
+		motor.Calc(decoi);
+
+		DrawMouse(m);
+		DrawMouse(decoi.Rotate(PI / 2);
+		motor.Draw();
+		DrawLineByDot(input.GetMouse(), center, BLUE);
+		DrawCircle(center.GetX(), center.GetY(), 3, RED, true);
+		
+	}
 
 	
 	DxLib_End();	// DXライブラリ終了処理
