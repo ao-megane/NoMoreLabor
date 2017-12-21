cc332c99f617d6509d1300958c0236635a30f450
荘太 小林 <org7uiqp@outlook.jp>
2017-12-21 15:58:11 +0900

    画像　ドットの判定

--- NoMoreLabor/NoMoreLabor/Chore.cpp
+++ NoMoreLabor/NoMoreLabor/Chore.cpp
@@ -5,3 +5,13 @@
 void DrawLineByDot(Dot a, Dot b,int color) {
 	DrawLine(a.GetX(), a.GetY(), b.GetX(), b.GetY(), color, false);
 }
+
+bool IsSafeInMap(Dot player, int imagehandle) {
+
+	if (out) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
