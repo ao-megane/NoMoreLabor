b1636679f5e22ad90dc0a61ad1f7b5165d20f12f
aomeg <aomegane0716@outlook.jp>
2018-02-23 18:19:03 +0900

    あうぇｒたうぇｒ

--- NoMoreLabor/NoMoreLabor/Wave.h
+++ NoMoreLabor/NoMoreLabor/Wave.h
@@ -3,19 +3,21 @@
 #include"Obj.h"
 #include"Value.h"
 
+//波に関する処理を統括するクラス
+
 class Wave
 {
 public:
 	Wave() {}
 	~Wave() {}
-	void Initialize();
-	void Set(int count);
-	int Updata(int count);
-	void Draw();
-	bool IsJump(Dot center);
+	void Initialize();		//初期化関数
+	void Set(int count);	//セッター関数
+	int Updata(int count);	//更新関数
+	void Draw();			//描画関数
+	bool IsJump(Dot center);//centerが波の中にいればtrue
 
 private:
-	Dot Point1;
+	Dot Point1;	//
 	Dot Point2;
 	bool dir_x;//波の進む方向がx軸方向ならtrue
 	double ang;
